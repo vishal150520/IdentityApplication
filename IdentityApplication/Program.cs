7a00bdcae21841fdd93df99d5ed5a67a7b4039c7
abhashk.sinha@scadea.com <abhashk.sinha@scadea.com>
2024-01-21 15:41:20 +0530

    Cors fix

--- IdentityApplication/Program.cs
+++ IdentityApplication/Program.cs
@@ -8,6 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("identitystr")));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
@@ -37,7 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowSpecificOrigin");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
